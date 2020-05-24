f8470a5f00460dd78e405f7e5b5689641cb98e5c
Federico Maria Guercilena <fguercilena@theorie.ikp.physik.tu-darmstadt.de>
2020-05-24 18:07:50 +0200

    Added the possibility to cross-reference files with ctags. This feels quite hacky, and there maybe no way to get it to really work flawlessly due to limitations of ctags itself

---
+++ modules/formatter.py
@@ -0,0 +1,44 @@
+from pygments.formatters import HtmlFormatter
+from pygments.util import get_bool_opt, get_list_opt
+from subprocess import run
+import json
+
+
+class EFormatter(HtmlFormatter):
+
+    def __init__(self, **options):
+        HtmlFormatter.__init__(self, **options)
+
+        if get_bool_opt(options, "ctags", False):
+
+            files = get_list_opt(options, "files", [])
+
+            try:
+                command = "ctags"
+                options = ["--fields='+n'", "--output-format=json", "-f -"]
+                tagsfile = run([command, *options, *files],
+                               capture_output=True)
+                tagsfile = tagsfile.stdout.decode("utf-8")
+                tagsfile = "[" + tagsfile.replace("\n", ",")[:-1] + "]"
+                data = json.loads(tagsfile)
+
+            except Exception as e:
+                print(e)
+                raise
+
+            tags = {}
+            for tag in data:
+                if tag["_type"] == "tag":
+                    tags[tag["name"]] = tag
+                    del tags[tag["name"]]["name"]
+
+            self._ctags = tags
+            self.tagurlformat = "%(fname)s.html"
+            self.tagsfile = "internal"
+
+    def _lookup_ctag(self, token):
+
+        if token in self._ctags:
+            return self._ctags[token]["path"], self._ctags[token]["line"]
+        else:
+            return None, None
